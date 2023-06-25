8353a32b493bdbe0fe6c184e6111e9e6d7087e11
Eranmonnie <ajalaferanmi123@gmail.com>
2023-06-25 15:58:44 +0100

    fake json server fatch request

--- src/Home.js
+++ src/Home.js
@@ -1,25 +1,43 @@
 
-import {useState} from 'react';
+import {useState, useEffect} from 'react';
 import BlogList from './BlogList';
 
 const Home = () => {
     
-    const [blogs, setBlogs] = useState([
-
-        {name:'feranmi', title:'the first dawn', date:'10-11-23' ,id:1},
-        {name:'taiwo', title:'the kush iu', date:'10-1-23' ,id:2},
-        {name:'ferjganmi', title:'tpijhe filjhrst dawn', date:'10-6-23', id:3},
-    ]);
+    const [blogs, setBlogs] = useState(null);
+    const [isPending, setIspending] = useState(true);
+    const [errors, setErrors] = useState(null);
 
     const handelDelete = (id)=>{
         const newarr = blogs.filter((blog)=> blog.id !== id );
         setBlogs(newarr);
-    }
+    };
+
+    useEffect(()=>{
+        fetch('http://localhost:8000/blogs')
+            .then(res=>{
+                if (! res.ok){
+                   throw Error(`could not fetch data for this resourse`);
+                }
+                return res.json()
+            })
+                .then(data=>{
+                    setIspending(false);
+                    setErrors(null);
+                    setBlogs(data);   
+                })
+                    .catch(err=>{ 
+                       setIspending(false);
+                       setErrors(err.message);            
+                    });
+    },[]);
 
     
     return ( 
         <div className="home">
-          <BlogList blogs={blogs} handelDelete={handelDelete}/>
+          {errors && <div>{errors}</div>}
+          {isPending && <div>Loading...</div>}
+          { blogs && <BlogList blogs={blogs} handelDelete={handelDelete}/>}
         </div>
      );
 }
