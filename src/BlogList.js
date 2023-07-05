7e89a47bc2c0086b53c9d1bd82665cd94ffc7a63
Eranmonnie <ajalaferanmi123@gmail.com>
2023-07-05 22:23:07 +0100

    rout params from url

--- src/BlogList.js
+++ src/BlogList.js
@@ -7,16 +7,13 @@ const blogs = props.blogs
     return ( 
         <div className="blogList">
             {blogs.map((blog)=>{ return(
-
-            <Link to={`blogs/${blog.id}`}>
                 <div className="blogs" key={blog.id}>
-                    <h2>{blog.title}</h2>
-                    <p>written by {blog.name}</p>
+                    <Link to={`blogs/${blog.id}`}> 
+                        <h2>{blog.title}</h2>
+                        <p>written by {blog.name}</p>
+                    </Link>
                 </div>
-
-            </Link>
         ); })}
-
         </div>
 
      );
