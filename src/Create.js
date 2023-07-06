0259b08940243c39b2838d92b672f9177a1abbd6
Eranmonnie <ajalaferanmi123@gmail.com>
2023-07-06 09:20:24 +0100

    two way data binding

--- src/Create.js
+++ src/Create.js
@@ -1,4 +1,8 @@
 const Create = () => {
+    const [title, setTitle] = useState('');
+    const [body, setBody] = useState('');
+    const [user, setUser] = useState('');
+
     return ( 
         <div className="Create">
             <h1 className="formdata">Create page</h1>
@@ -9,15 +13,24 @@ const Create = () => {
                     <input 
                     type="text"
                     required
+                    value={title}
+                    onChange={(e)=>{setTitle(e.target.value);
+                    }}
                      />
 
                     <label>Body</label>
                     <textarea 
                     required
+                    value={body}
+                    onChange={(e)=>{setBody(e.target.value);
+                    }}
                     />
 
                     <label>User</label>
-                    <select>
+                    <select
+                     value={user}
+                     onChange={(e)=>{setUser(e.target.value);
+                     }}>
                         <option value="mario">mario</option>
                         <option value="yoshi">yoshi</option>
                     </select>
